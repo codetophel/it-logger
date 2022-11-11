0b5c8fa13dd21cbc65d0a6a0a3cf6b3fdadf5618
codetophel <nicodemusakande@gmail.com>
2022-11-11 13:47:58 +0100

    destructuring and project completion

--- src/components/logs/LogsItem.js
+++ src/components/logs/LogsItem.js
@@ -6,8 +6,9 @@ import Moment from 'react-moment';
 import { deleteLog, setCurrent } from '../../actions/logActions';
 
 const LogsItem = ({ log, deleteLog, setCurrent }) => {
+  const { id, attention, message, tech, date } = log;
   const onDelete = () => {
-    deleteLog(log.id);
+    deleteLog(id);
 
     M.toast({
       html: `Log deleted `,
@@ -18,19 +19,16 @@ const LogsItem = ({ log, deleteLog, setCurrent }) => {
       <div>
         <a
           href='#edit-log-modal'
-          className={`modal-trigger ${
-            log.attention ? 'red-text' : 'blue-text'
-          }`}
+          className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}
           onClick={() => setCurrent(log)}
         >
-          {log.message}
+          {message}
         </a>
         <br />
         <span className='grey-text'>
-          <span className='black-text'>ID #{log.id}</span> last updated by{' '}
-          {'  '}
-          <span className='black-text'>{log.tech}</span> on {'  '}{' '}
-          <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
+          <span className='black-text'>ID #{id}</span> last updated by {'  '}
+          <span className='black-text'>{tech}</span> on {'  '}{' '}
+          <Moment format='MMMM Do YYYY, h:mm:ss a'>{date}</Moment>
         </span>
         <a href='#!' className='secondary-content' onClick={onDelete}>
           <i className='material-icons grey-text'>delete</i>
--- src/components/techs/TechListModalItem.js
+++ src/components/techs/TechListModalItem.js
@@ -5,17 +5,18 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import { deleteTech } from '../../actions/techActions';
 
 const TechListModalItem = ({ tech, deleteTech }) => {
+  const { firstName, lastName, id } = tech;
   const onDelete = () => {
-    deleteTech(tech.id);
+    deleteTech(id);
 
     M.toast({
-      html: `Tech deleted `,
+      html: `${firstName} ${lastName} was deleted from the technician list `,
     });
   };
   return (
     <li className='collection-item'>
       <div>
-        {tech.firstName} {tech.lastName}
+        {firstName} {lastName}
         <a href='#!' className='secondary-content' onClick={onDelete}>
           <i className='material-icons grey-text'>delete</i>
         </a>
